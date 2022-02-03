744da43360a088cb76e39f0d40af9c44ec82d8b6
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2022-02-02 17:17:24 -0800

    updated

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -23,6 +23,14 @@ public class MarkdownParseTest {
         List<String> expectedList = List.of("https://something.com", "some-page.html");
         assertEquals(expectedList, links);
     }
+    @Test
+    public void testGetLink() throws IOException{
+        Path fileName = Path.of("test1.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        List<String> expectedList = List.of("ucsd.edu");
+        assertEquals(expectedList, links);
+    }
 
     // @Test
     // public void testGetMoreLinks() throws IOException{
---
+++ test1.md
@@ -0,0 +1,1 @@
+[UCSD](ucsd.edu)
